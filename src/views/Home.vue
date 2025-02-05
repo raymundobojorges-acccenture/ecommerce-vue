e51ced8915cc54c12249bdc03af2886bec7fa6db
Raymundo Bojorges <raymundo.bojorges@accenture.com>
2025-02-05 00:56:21 -0600

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1 class="font-bold text-2xl text-center mt-5">Vue Ecommerce</h1>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+
+  </script>
+  
+  <style scoped>
+  </style>
+  
